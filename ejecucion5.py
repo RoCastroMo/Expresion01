405797b8bf088118f40d37b5feb57729cae309a1
RoCastroMo <ro.castromo01@gmail.com>
2022-10-24 04:04:01 -0300

    script para calcular descuentos de un rent a car segun dias

---
+++ ejecucion5.py
@@ -0,0 +1,18 @@
+def rental_car_cost(d):
+    rent = 40
+    if d == 1:
+        return rent
+    if d >= 7:
+        rent *= d
+        result2 = rent - 50
+        return result2
+    if d >= 3:
+        rent *= d
+        result1 = rent - 20
+        return result1
+
+
+print(rental_car_cost(1))
+print(rental_car_cost(4))
+print(rental_car_cost(7))
+print(rental_car_cost(8))
