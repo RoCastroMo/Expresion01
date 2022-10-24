ea8333c156711306f6aa6153ef7a29e71a2292e5
RoCastroMo <ro.castromo01@gmail.com>
2022-10-24 00:54:22 -0300

    script para obtener las iniciales de nombre y apellido

--- ejecucion4.py
+++ ejecucion4.py
@@ -1,11 +1,13 @@
+fname = "Rodolfo"
+lname = "Castro"
+name = fname + ' ' + lname
 
-def abbrev_name(name):
+
+def abbrev_name( name ):
     last = name.split()
     first = last[0]
     las = last[1]
     return f'{first[0].upper()}.{las[0].upper()}'
-fname = "Rodolfo"
-lname= "Castro"
-name = fname +' '+ lname
+
 
 print(abbrev_name(name))
