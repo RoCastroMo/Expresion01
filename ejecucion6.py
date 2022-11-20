0f7fcb8b91f31f4dec3f06403792e6da274abbd1
RoCastroMo <ro.castromo01@gmail.com>
2022-11-19 22:50:15 -0300

    script para contar la cantidad de petalos

---
+++ ejecucion6.py
@@ -0,0 +1,13 @@
+#Cantidad de petalos
+def how_much_i_love_you(nb_petals):
+    if nb_petals == 3:
+        return "a lot"
+    elif nb_petals == 7:
+        return "I love you"
+    elif nb_petals == 6:
+        return "not at all"
+
+
+print(how_much_i_love_you(7))
+print(how_much_i_love_you(3))
+print(how_much_i_love_you(6))
